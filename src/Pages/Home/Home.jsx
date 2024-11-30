875412663c3958eeaf998e6a3d0684783f6e05c0
beyeberu <beyeberueyayu@gmail.com>
2024-11-30 04:09:24 -0800

    i see

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/Banner";
 import Rowlist from "../../Components/Rows/RowList/Rowlist";
-
+//abrish i am beyu enem alhubet
 const Home = () => {
   return (
     <>
